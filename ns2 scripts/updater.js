0c196b72667ae69e092da450181a10a7fa336561
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-22 03:28:02 +1030

    updated to work with local webserver

--- ns2 scripts/updater.js
+++ ns2 scripts/updater.js
@@ -1,8 +1,7 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    let url =
-            'https://raw.githubusercontent.com/willhollingsworth/Bitburner/dev/ns2%20scripts/scanner.js',
-        filename = 'scanner.js';
-    ns.wget(url, filename);
-    ns.exec(filename);
+    let url = 'http://127.0.0.1:7000/';
+    await ns.wget(url + ns.args[0], ns.args[0] + '.js', 'home');
+    await ns.sleep(500);
+    await ns.exec(ns.args[0] + '.js', 'home');
 }
