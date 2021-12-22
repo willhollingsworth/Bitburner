b8516697c132afa257abe98174dcf31067941746
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-22 18:31:08 +1030

    first ver of bb script runner

---
+++ tools/bitburner_script_runner.py
@@ -0,0 +1,38 @@
+import keyboard
+import win32gui
+from time import sleep
+
+"""
+    rebinds the default keyboard shortcut for running in vscode (with coderunner extension installed) 
+    to execute the script in bitburner
+
+https://github.com/boppreh/keyboard
+https://github.com/mhammond/pywin32
+
+"""
+
+
+def switch_bitburner():
+    current_title = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    current_script = current_title.split(".")[0]
+    print(current_script)
+    sleep(0.2)
+    # switch to bitburner window
+    win32gui.SetForegroundWindow(win32gui.FindWindow(None, "Bitburner"))
+
+    # sleep(0.2)
+    # keyboard.press_and_release("alt+t")
+    sleep(0.2)
+    keyboard.write("run updater.js " + current_script)
+    sleep(0.2)
+    keyboard.press_and_release("enter")
+
+
+keyboard.add_hotkey(
+    "ctrl + alt + n",
+    switch_bitburner,
+    suppress=True,
+)
+
+
+keyboard.wait()  # leave program running awaiting the hotkey
