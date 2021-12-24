61c9c4b3b7e22c15ba27ebb818be132dc2bbc5bb
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-24 16:29:15 +1030

    updater passes an argument to new program

--- ns2 scripts/updater.js
+++ ns2 scripts/updater.js
@@ -2,6 +2,13 @@
 export async function main(ns) {
     let url = 'http://127.0.0.1:7000/';
     await ns.wget(url + ns.args[0], ns.args[0] + '.js', 'home');
+    if (ns.getScriptName() == ns.args[0] + '.js') {
+        ns.tprint('self updating detected, will not run exec command');
+        ns.exit();
+    }
     await ns.sleep(500);
-    await ns.exec(ns.args[0] + '.js', 'home');
+    if (!ns.args[1]) {
+        ns.args[1] = '';
+    }
+    await ns.exec(ns.args[0] + '.js', 'home', 1, ns.args[1]);
 }
