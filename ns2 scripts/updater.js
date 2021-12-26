0d1022a690cdfd09f308794adb71fe225f1f7c81
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-26 20:27:18 +1030

    ensure updater kills old script

--- ns2 scripts/updater.js
+++ ns2 scripts/updater.js
@@ -1,12 +1,14 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    let url = 'http://127.0.0.1:7000/';
-    if (ns.getScriptName() == ns.args[0] + '.js' || !ns.args[0]) {
+    let url = 'http://127.0.0.1:7000/',
+        script = ns.args[0];
+
+    if (ns.getScriptName() == script + '.js' || !script) {
         await ns.wget(url + 'updater', 'updater.js', 'home');
         ns.tprint('self updating detected, will not run exec command');
         ns.exit();
     }
-    await ns.wget(url + ns.args[0], ns.args[0] + '.js', 'home');
+    await ns.wget(url + script, script + '.js', 'home');
     await ns.sleep(500);
     // if (!ns.args[1]) {
     //     ns.args[1] = '';
@@ -14,23 +16,17 @@ export async function main(ns) {
     // if (!ns.args[2]) {
     //     ns.args[2] = '';
     // }
-
+    ns.scriptKill(script, 'home');
     if (ns.args[1]) {
         if (ns.args[2]) {
             ns.tprint('2 args - ', ns.args[1], ', ', ns.args[2]);
-            await ns.exec(
-                ns.args[0] + '.js',
-                'home',
-                1,
-                ns.args[1],
-                ns.args[2]
-            );
+            await ns.exec(script + '.js', 'home', 1, ns.args[1], ns.args[2]);
             ns.exit();
         }
         ns.tprint('1 args - ', ns.args[1]);
-        await ns.exec(ns.args[0] + '.js', 'home', 1, ns.args[1]);
+        await ns.exec(script + '.js', 'home', 1, ns.args[1]);
     } else {
         ns.tprint('no args');
-        await ns.exec(ns.args[0] + '.js', 'home', 1);
+        await ns.exec(script + '.js', 'home', 1);
     }
 }
