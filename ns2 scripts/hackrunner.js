b4af1d19b38f00fb115bb5d4a5a3339e0d11900a
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-29 16:00:45 +1030

    hrun better logging, access, running

--- ns2 scripts/hackrunner.js
+++ ns2 scripts/hackrunner.js
@@ -6,9 +6,17 @@ import {
     calc_hack_amount,
 } from 'server_info.js';
 
-export async function log_to_file(ns, data) {
+export async function csv_log(ns, data) {
     let filename = ns.getScriptName().split('.')[0];
     filename = 'log_csv_' + filename + '.txt';
+    data = data.join(', ');
+    await ns.write(filename, data);
+    await ns.write(filename, '\r\n');
+}
+
+export async function log(ns, data) {
+    let filename = ns.getScriptName().split('.')[0];
+    filename = 'log_' + filename + '.txt';
     data = data.join(', ');
     await ns.write(filename, data);
     await ns.write(filename, '\r\n');
@@ -26,6 +34,18 @@ export function check_and_get_access(ns, target) {
     }
     if (ns.fileExists('FTPCrack.exe', 'home')) {
         ns.ftpcrack(target);
+        num_ports_avail += 1;
+    }
+    if (ns.fileExists('relaySMTP.exe', 'home')) {
+        ns.relaysmtp(target);
+        num_ports_avail += 1;
+    }
+    if (ns.fileExists('HTTPWorm.exe', 'home')) {
+        ns.httpworm(target);
+        num_ports_avail += 1;
+    }
+    if (ns.fileExists('SQLInject.exe', 'home')) {
+        ns.sqlinject(target);
         num_ports_avail += 1;
     }
     let port_delta = num_ports_avail - num_ports_req;
@@ -48,10 +68,12 @@ export function build_targets_object(ns, hosts) {
     return output_object;
 }
 
-export function find_hosts(ns, ignored_hosts) {
-    let hosts = run_scan(ns, 'home', 3); // build an array of directly and indirectly connected hosts
-    hosts = hosts.filter((host) => !ignored_hosts.includes(host)); //filter unwanted hosts
+export function find_hosts(ns, ignored_hosts = false) {
+    let hosts = run_scan(ns, 'home', 5); // build an array of directly and indirectly connected hosts
     hosts.push('home');
+    if (ignored_hosts) {
+        hosts = hosts.filter((host) => !ignored_hosts.includes(host)); //filter unwanted hosts
+    }
     return hosts;
 }
 
@@ -70,17 +92,19 @@ export function get_target_info(ns, target) {
 }
 
 export function run_script(ns, target, script, threads) {
-    let servers = ns.getPurchasedServers();
+    let servers = find_hosts(ns),
+        reserved_ram = 10;
     script += '.script';
     let required_ram = ns.getScriptRam(script) * threads;
-    // if (!check_and_get_access(ns, target)) {
-    //     continue;
-    // }
-    // available_ram =
-    //     ns.getServerMaxRam(host) - ns.getServerUsedRam(host);
 
     for (let server of servers) {
+        if (!check_and_get_access(ns, server)) {
+            continue;
+        }
         let free_ram = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+        if (server == 'host') {
+            free_ram -= reserved_ram;
+        }
         if (free_ram < required_ram) {
             continue;
         }
@@ -88,6 +112,9 @@ export function run_script(ns, target, script, threads) {
         // if (!ns.fileExists(script, server)) {
         //     await ns.scp(script, 'home', server);
         // }
+        if (!check_and_get_access(ns, target)) {
+            continue;
+        }
         ns.exec(script, server, threads, target); // run the script}
     }
 
@@ -118,35 +145,26 @@ export async function main(ns) {
     }
 
     //print out headers
-    await log_to_file(ns, [
-        'host',
-        'script',
-        'money_percent',
-        'security_delta',
-        'threads',
-    ]);
+    await csv_log(ns, ['target', 'script', 'threads', 'current']);
     let targets = build_targets_object(ns, hosts);
 
     while (true) {
         hosts = find_hosts(ns, ignored_hosts);
-        // ns.tprint(targets);
         for (let target in targets) {
-            // loop over each host
+            // loop over each target
             await ns.sleep(500);
             let [security_delta, money_percent] = get_target_info(ns, target);
-            // ns.tprint(target, ' - ', security_delta, ' - ', money_percent);
 
             // if security is too high and no active weaken tasks
             if (security_delta > 0) {
                 if (targets[target][0] == 0) {
                     let weakens_required = calc_weaken_amount(ns, target);
-                    ns.tprint(
+                    await csv_log(ns, [
                         target,
-                        ' needs ',
+                        'weaken',
                         weakens_required,
-                        ' weakens, currently running ',
-                        targets[target]
-                    );
+                        targets[target],
+                    ]);
                     run_script(ns, target, 'weaken', weakens_required);
                     targets[target] = [weakens_required, 0, 0];
                 }
@@ -154,26 +172,24 @@ export async function main(ns) {
                 if (targets[target][1] == 0) {
                     //if money too low and now active grows running
                     let grows_required = calc_growth_amount(ns, target);
-                    ns.tprint(
+                    await csv_log(ns, [
                         target,
-                        ' needs ',
+                        'grow',
                         grows_required,
-                        ' grows, currently running ',
-                        targets[target]
-                    );
+                        targets[target],
+                    ]);
                     run_script(ns, target, 'grow', grows_required);
                     targets[target] = [0, grows_required, 0];
                 }
             } else {
                 if (targets[target][2] == 0) {
                     let hacks_required = calc_hack_amount(ns, target, 50);
-                    ns.tprint(
+                    await csv_log(ns, [
                         target,
-                        ' needs ',
+                        'hack',
                         hacks_required,
-                        ' hacks, currently running ',
-                        targets[target]
-                    );
+                        targets[target],
+                    ]);
                     run_script(ns, target, 'hack', hacks_required);
                     targets[target] = [0, 0, hacks_required];
                 }
