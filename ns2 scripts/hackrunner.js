fd0a9df1fa30697da7c388ee98615a28079d0d14
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-29 18:48:33 +1030

    hrun fixed async issues + better logging

--- ns2 scripts/hackrunner.js
+++ ns2 scripts/hackrunner.js
@@ -6,20 +6,28 @@ import {
     calc_hack_amount,
 } from 'server_info.js';
 
-export async function csv_log(ns, data) {
-    let filename = ns.getScriptName().split('.')[0];
-    filename = 'log_csv_' + filename + '.txt';
-    data = data.join(', ');
-    await ns.write(filename, data);
-    await ns.write(filename, '\r\n');
+export async function csv_log(ns, data, debug) {
+    await write_csv(ns, data);
+    if (debug) {
+        await write_csv(ns, data, 'debug_');
+    }
 }
 
-export async function log(ns, data) {
-    let filename = ns.getScriptName().split('.')[0];
-    filename = 'log_' + filename + '.txt';
+export async function debug_log(ns, data, debug) {
+    if (debug) {
+        await write_csv(ns, data, 'debug_');
+    }
+}
+
+export async function write_csv(ns, data, filename_mod = '') {
+    let filename = ns.getScriptName().split('.')[0],
+        timestamp = Date().split(' ')[4];
+    filename = filename_mod + 'log_csv_' + filename + '.txt';
+    data.unshift(timestamp);
     data = data.join(', ');
     await ns.write(filename, data);
     await ns.write(filename, '\r\n');
+    return;
 }
 
 export function check_and_get_access(ns, target) {
@@ -91,12 +99,22 @@ export function get_target_info(ns, target) {
     return [security_delta, money_percent];
 }
 
-export function run_script(ns, target, script, threads) {
+export async function run_script(ns, target, script, threads, debug) {
     let servers = find_hosts(ns),
         reserved_ram = 10;
-    script += '.script';
+    script += '.js';
     let required_ram = ns.getScriptRam(script) * threads;
-
+    if (threads < 1 || isNaN(threads)) {
+        ns.tprint(
+            'run script function fed NaN, target:',
+            target,
+            ' , script: ',
+            script,
+            ' , threads: ',
+            threads
+        );
+        return;
+    }
     for (let server of servers) {
         if (!check_and_get_access(ns, server)) {
             continue;
@@ -110,14 +128,23 @@ export function run_script(ns, target, script, threads) {
         }
         // deploy script to server
         // if (!ns.fileExists(script, server)) {
-        //     await ns.scp(script, 'home', server);
+        await debug_log(ns, ['scp script', script, 'to', server], debug);
+        await ns.scp(script, 'home', server);
         // }
         if (!check_and_get_access(ns, target)) {
             continue;
         }
-        ns.exec(script, server, threads, target); // run the script}
-    }
+        if (!ns.exec(script, server, threads, target)) {
+            ns.tprint(script, ' Failed to run on ', target);
+        } // run the script
 
+        return;
+    }
+    await csv_log(
+        ns,
+        ['--------- unable to find server with enough ram ---------'],
+        debug
+    );
     return;
 }
 
@@ -132,24 +159,31 @@ export async function main(ns) {
         security_delta_predict = 0,
         money_percent_predict = 0,
         available_ram = 0,
-        log = true,
+        debug = true,
+        log_details = [],
         hosts = find_hosts(ns, ignored_hosts);
-    if (ns.args[0] == 'log') {
-        log = true;
-    } else if (ns.args[0] == 'kill') {
-        // if kill argument then shut down all active programs via a kill all
-        var k = true;
-    }
-    if (log) {
-        ns.rm(ns.getScriptName().split('.')[0], 'home');
-    }
 
     //print out headers
-    await csv_log(ns, ['target', 'script', 'threads', 'current']);
+    await csv_log(ns, ['--------------------------------------------'], debug);
+    await csv_log(ns, ['------------ Script starting up-------------'], debug);
+    await csv_log(ns, ['--------------------------------------------'], debug);
+    await csv_log(
+        ns,
+        [
+            'target',
+            'script',
+            'threads',
+            'current',
+            'security_delta',
+            'money_percent',
+        ],
+        debug
+    );
     let targets = build_targets_object(ns, hosts);
 
     while (true) {
         hosts = find_hosts(ns, ignored_hosts);
+        await debug_log(ns, ['hosts: ', ...hosts], debug);
         for (let target in targets) {
             // loop over each target
             await ns.sleep(500);
@@ -159,51 +193,86 @@ export async function main(ns) {
             if (security_delta > 0) {
                 if (targets[target][0] == 0) {
                     let weakens_required = calc_weaken_amount(ns, target);
-                    await csv_log(ns, [
+                    log_details = [
                         target,
                         'weaken',
                         weakens_required,
                         targets[target],
-                    ]);
-                    run_script(ns, target, 'weaken', weakens_required);
+                        security_delta,
+                        money_percent,
+                    ];
+                    await csv_log(ns, log_details, debug);
+                    await run_script(
+                        ns,
+                        target,
+                        'weaken',
+                        weakens_required,
+                        debug
+                    );
                     targets[target] = [weakens_required, 0, 0];
+                } else {
+                    await debug_log(
+                        ns,
+                        [
+                            target,
+                            'security too high:',
+                            security_delta,
+                            'weaken already being run',
+                            targets[target],
+                        ],
+                        debug
+                    );
                 }
             } else if (money_percent < 100) {
                 if (targets[target][1] == 0) {
                     //if money too low and now active grows running
                     let grows_required = calc_growth_amount(ns, target);
-                    await csv_log(ns, [
+                    log_details = [
                         target,
                         'grow',
                         grows_required,
                         targets[target],
-                    ]);
-                    run_script(ns, target, 'grow', grows_required);
+                        security_delta,
+                        money_percent,
+                    ];
+                    await csv_log(ns, log_details, debug);
+                    await run_script(ns, target, 'grow', grows_required);
                     targets[target] = [0, grows_required, 0];
+                } else {
+                    await debug_log(
+                        ns,
+                        [
+                            target,
+                            'money too low:',
+                            money_percent,
+                            'grow already being run',
+                            targets[target],
+                        ],
+                        debug
+                    );
                 }
             } else {
                 if (targets[target][2] == 0) {
-                    let hacks_required = calc_hack_amount(ns, target, 50);
-                    await csv_log(ns, [
+                    let hacks_required = calc_hack_amount(ns, target, 90);
+                    log_details = [
                         target,
                         'hack',
                         hacks_required,
                         targets[target],
-                    ]);
-                    run_script(ns, target, 'hack', hacks_required);
+                        security_delta,
+                        money_percent,
+                    ];
+                    await csv_log(ns, log_details, debug);
+                    await run_script(ns, target, 'hack', hacks_required);
                     targets[target] = [0, 0, hacks_required];
+                } else {
+                    await debug_log(
+                        ns,
+                        [target, 'hack already being run', targets[target]],
+                        debug
+                    );
                 }
             }
-
-            // // determine the number of times the script can be run
-            // threads = Math.floor(available_ram / ns.getScriptRam(script, host));
-
-            // // leave ram free on home
-            // if (host == 'home') {
-            //     threads = Math.floor(
-            //         (available_ram - 10) / ns.getScriptRam(script, host)
-            //     );
-            // }
         }
     }
 }
