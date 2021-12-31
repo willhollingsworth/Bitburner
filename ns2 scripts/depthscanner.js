eda87561f3ec2c1eeddaaa9955f0167882433c03
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2021-12-31 18:03:09 +1030

    dscan debug options

--- ns2 scripts/depthscanner.js
+++ ns2 scripts/depthscanner.js
@@ -15,11 +15,13 @@ export function return_new_neighbors(ns, hosts) {
     return output;
 }
 
-export function run_scan(ns, host = 'home', depth = 1) {
+export function run_scan(ns, host = 'home', depth = 1, debug = false) {
     var hosts = ns.scan(host);
+    ns.tprint('first scan hosts : ', hosts.length);
     for (var i of [...Array(depth - 1).keys()]) {
         // ns.tprint('loop ', i, ', hosts ', hosts);
         hosts = return_new_neighbors(ns, hosts).concat(hosts);
+        ns.tprint('iter : ', i, ' hosts : ', hosts.length);
     }
 
     // ns.tprint('scan run from ' + host + ', d:' + depth + ', result: ' + hosts);
@@ -29,13 +31,14 @@ export function run_scan(ns, host = 'home', depth = 1) {
 export async function main(ns) {
     let depth = 2,
         args = ns.args[0],
-        scan_results = 0;
+        scan_results = 0,
+        debug = true;
 
     if (args) {
         depth = args;
     }
 
-    scan_results = run_scan(ns, 'home', depth);
+    scan_results = run_scan(ns, 'home', depth, debug);
     ns.tprint('scan ran with depth : ', depth);
     ns.tprint('results : ', scan_results.length);
     ns.tprint(scan_results);
