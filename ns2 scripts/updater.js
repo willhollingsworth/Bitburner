f53b95a7acff2dffc907bbe05446e50c97d950f1
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-01-02 11:55:19 +1030

    updater better rerunning

--- ns2 scripts/updater.js
+++ ns2 scripts/updater.js
@@ -1,7 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     let url = 'http://127.0.0.1:7000/',
-        script = ns.args[0];
+        script = ns.args[0],
+        scriptJs = script + '.js';
     if (script) {
         if (script.includes('.js')) {
             ns.tprintf('INFO - js found');
@@ -9,12 +10,12 @@ export async function main(ns) {
         }
     }
 
-    if (ns.getScriptName() == script + '.js' || !script) {
+    if (ns.getScriptName() == scriptJs || !script) {
         await ns.wget(url + 'updater', 'updater.js', 'home');
         ns.tprintf('WARN - self updating detected, will not run exec command');
         ns.exit();
     }
-    if (await ns.wget(url + script, script + '.js', 'home')) {
+    if (await ns.wget(url + script, scriptJs, 'home')) {
         ns.tprint(script, ' downloaded');
     } else {
         ns.tprintf('ERROR - ' + script + ' unable to download!');
@@ -26,17 +27,21 @@ export async function main(ns) {
     // if (!ns.args[2]) {
     //     ns.args[2] = '';
     // }
-    ns.scriptKill(script, 'home');
+    if (ns.isRunning(scriptJs, 'home')) {
+        ns.tprintf('WARNING - old script still running, killing it');
+        ns.scriptKill(scriptJs, 'home');
+        await ns.sleep(1000);
+    }
     if (ns.args[1]) {
         if (ns.args[2]) {
             ns.tprint('2 args - ', ns.args[1], ', ', ns.args[2]);
-            await ns.exec(script + '.js', 'home', 1, ns.args[1], ns.args[2]);
+            await ns.exec(scriptJs, 'home', 1, ns.args[1], ns.args[2]);
             ns.exit();
         }
         ns.tprint('1 args - ', ns.args[1]);
-        await ns.exec(script + '.js', 'home', 1, ns.args[1]);
+        await ns.exec(scriptJs, 'home', 1, ns.args[1]);
     } else {
         ns.tprint('no args');
-        await ns.exec(script + '.js', 'home', 1);
+        await ns.exec(scriptJs, 'home', 1);
     }
 }
