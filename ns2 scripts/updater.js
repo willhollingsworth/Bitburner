c4cc214b76f20b43b24f3dfa516e7e5931160a9e
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-01-02 11:46:33 +1030

    updater added small features

--- ns2 scripts/updater.js
+++ ns2 scripts/updater.js
@@ -2,13 +2,23 @@
 export async function main(ns) {
     let url = 'http://127.0.0.1:7000/',
         script = ns.args[0];
+    if (script) {
+        if (script.includes('.js')) {
+            ns.tprintf('INFO - js found');
+            script = script.split('.')[0];
+        }
+    }
 
     if (ns.getScriptName() == script + '.js' || !script) {
         await ns.wget(url + 'updater', 'updater.js', 'home');
-        ns.tprint('self updating detected, will not run exec command');
+        ns.tprintf('WARN - self updating detected, will not run exec command');
         ns.exit();
     }
-    await ns.wget(url + script, script + '.js', 'home');
+    if (await ns.wget(url + script, script + '.js', 'home')) {
+        ns.tprint(script, ' downloaded');
+    } else {
+        ns.tprintf('ERROR - ' + script + ' unable to download!');
+    }
     await ns.sleep(500);
     // if (!ns.args[1]) {
     //     ns.args[1] = '';
