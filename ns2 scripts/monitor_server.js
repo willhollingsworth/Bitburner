dde47593ccb30627739e9333052fc629d00dfa97
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-01-05 01:20:47 +1030

    server monitor more detailed live display

--- ns2 scripts/monitor_server.js
+++ ns2 scripts/monitor_server.js
@@ -1,28 +1,59 @@
 /** @param {NS} ns **/
 import { get_server_info } from 'server_info.js';
 export async function main(ns) {
-    let target = 'n00dles',
+    let target = ns.args[0],
         log_file = 'monitor_' + target + '.txt',
+        action = '',
+        time = '',
         server_info = {},
-        useful_details = [],
+        current_details = [],
         previous_details = [];
+
+    if (!target) {
+        target = 'n00dles';
+    }
 
     while (true) {
         server_info = get_server_info(ns, target, 'all');
-        previous_details = useful_details;
-        useful_details = [
+        previous_details = current_details;
+        current_details = [
             server_info.security_delta[1],
             server_info.money_percent[1],
         ];
 
         if (
-            useful_details[0] != previous_details[0] ||
-            useful_details[1] != previous_details[1]
+            current_details[0] != previous_details[0] ||
+            current_details[1] != previous_details[1]
         ) {
-            ns.tprint(target, ' ', useful_details);
+            if (current_details[0] < previous_details[0]) {
+                action = 'weaken';
+            } else if (current_details[1] > previous_details[1]) {
+                action = 'hack  ';
+            } else if (current_details[1] < previous_details[1]) {
+                action = 'grow  ';
+            } else {
+                action = 'unsure';
+            }
+            if (time != '') {
+                time = (new Date() / 1000 - time).toFixed(1);
+                ns.tprint(
+                    Date().split(' ')[4],
+                    ' ',
+                    target,
+                    ' ',
+                    action,
+                    '  tDelta ',
+                    time,
+                    '   ',
+                    current_details[0],
+                    ' ',
+                    current_details[1]
+                );
+            }
+            time = new Date() / 1000;
         }
-        await ns.sleep(100);
+        await ns.sleep(50);
     }
 
-    ns.tprint(useful_details);
+    ns.tprint(current_details);
 }
